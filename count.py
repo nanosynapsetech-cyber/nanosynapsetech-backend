58a97fc818103f532fc20654e4e432fb2d4edc6c
nanosynapsetech <nanosynapsetech@gmail.com>
2026-06-10 22:18:35 +0300

    chore: remove 19 test/debug/scratch files from backend

--- count.py
+++
@@ -1,4 +0,0 @@
-import sqlite3
-c = sqlite3.connect('database.db').cursor()
-c.execute("SELECT COUNT(1) FROM genes WHERE Biotype LIKE '%mrna%' OR Biotype = 'transcript' OR Biotype LIKE '%cdna%'")
-print(c.fetchone()[0])
--- test_fts.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect(':memory:')
-conn.execute('CREATE TABLE genes (id INTEGER PRIMARY KEY, seq TEXT)')
-conn.execute("INSERT INTO genes (seq) VALUES ('AGCUUAACCGG'), ('CCGGAGCUUAA'), ('AAAAA')")
-conn.execute("CREATE VIRTUAL TABLE genes_fts USING fts5(seq, content='genes', content_rowid='id', tokenize='trigram')")
-conn.execute("INSERT INTO genes_fts(genes_fts) VALUES('rebuild')")
-res = conn.execute("SELECT genes.id, genes.seq FROM genes JOIN genes_fts ON genes.id = genes_fts.rowid WHERE genes_fts.seq MATCH '\"AGCUUAA\"'").fetchall()
-print(res)
